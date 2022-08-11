db07b26e45f0734b0c9fec47d6c3e86db8cf8499
Alex Sedmoy <alexsedmoy@gmail.com>
2022-08-11 23:40:29 +0300

    Pagination - React + Redux

--- lesson4/task3/src/users/Pagination.jsx
+++ lesson4/task3/src/users/Pagination.jsx
@@ -1,25 +1,17 @@
 import React from 'react';
 
 const Pagination = ({ goPrev, goNext, currentPage, totalItems, itemsPerPage }) => {
-  console.log('{ currentPage, totalItems, itemsPerPage }: ', {
-    currentPage,
-    totalItems,
-    itemsPerPage,
-  });
-  console.log(Math.ceil(totalItems / itemsPerPage));
   let isPrevPageAvailable = true;
   let isNextPageAvailable = true;
   let textPrevBtn = '←';
   let textNextBtn = '→';
 
-  // currentPage = currentPage + 1;
-
-  if (totalItems === 0 || currentPage === 1) {
+  if (currentPage === 1) {
     isPrevPageAvailable = false;
     textPrevBtn = null;
   }
 
-  if (totalItems === 0 || currentPage === Math.ceil(totalItems / itemsPerPage)) {
+  if (currentPage === Math.ceil(totalItems / itemsPerPage)) {
     isNextPageAvailable = false;
     textNextBtn = null;
   }
@@ -30,14 +22,9 @@ const Pagination = ({ goPrev, goNext, currentPage, totalItems, itemsPerPage }) =
         {textPrevBtn}
       </button>
       <span className="pagination__page">{currentPage}</span>
-
-      {isNextPageAvailable ? (
-        <button className="btn" onClick={goNext}>
-          →
-        </button>
-      ) : (
-        <button className="btn" onClick={goNext} disabled></button>
-      )}
+      <button className="btn" onClick={goNext} disabled={!isNextPageAvailable}>
+        {textNextBtn}
+      </button>
     </div>
   );
 };
