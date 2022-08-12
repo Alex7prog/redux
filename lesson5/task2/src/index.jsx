306bc5e9abf1e87e6791247e1d8e3815c65d1023
Alex Sedmoy <alexsedmoy@gmail.com>
2022-08-12 17:01:56 +0300

    Transfer List

---
+++ lesson5/task2/src/index.jsx
@@ -0,0 +1,7 @@
+// Transfer List
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
