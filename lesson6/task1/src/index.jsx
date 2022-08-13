abdb1a698d4d7682a9a18b3eae0127e02c285c87
Alex Sedmoy <alexsedmoy@gmail.com>
2022-08-14 01:11:42 +0300

    Redux loger middleware - part 1

---
+++ lesson6/task1/src/index.jsx
@@ -0,0 +1,7 @@
+// Redux loger middleware - part 1
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
