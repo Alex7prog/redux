561f26170440353e26d8028d205e781c48d3eb5a
Alex Sedmoy <alexsedmoy@gmail.com>
2022-08-14 21:52:23 +0300

    Weather app - React + Redux

---
+++ lesson6/task3/src/index.jsx
@@ -0,0 +1,7 @@
+// Weather app - React + Redux
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
---
+++ lesson6/task3/src/weather/Weather.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import * as weatherActions from './weather.actions';
+import { weatherDataSelector } from './weather.selectors';
+
+export const Weather = ({ weatherData, fetchWeatherData }) => {
+  if (!weatherData) {
+    fetchWeatherData();
+    return null;
+  }
+
+  return (
+    <main className="weather">
+      <h1 className="weather__title">Weather data</h1>
+      <ul className="cities-list">
+        {weatherData.map(city => (
+          <li key={city.id} className="city">
+            <span className="city__name">{city.name}</span>
+            <span className="city__temperature">{`${city.temperature} F`}</span>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+Weather.propsTypes = {
+  weatherData: PropTypes.array.isRequired,
+  fetchWeatherData: PropTypes.func.isRequired,
+};
+
+const mapState = state => {
+  return {
+    weatherData: weatherDataSelector(state),
+  };
+};
+
+const mapDispatch = {
+  fetchWeatherData: weatherActions.fetchWeatherData,
+};
+
+export default connect(mapState, mapDispatch)(Weather);
